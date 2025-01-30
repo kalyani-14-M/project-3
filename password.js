70a8d7fcff514fa4090eb9d5c1c2c0eb6d99de29
kalyani-14-M <kalyanikalyani2445@gmail.com>
2025-01-30 23:03:47 +0530

    Add files via upload

---
+++ password.js
@@ -0,0 +1,38 @@
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const includeUppercase = document.getElementById("uppercase").checked;
+    const includeLowercase = document.getElementById("lowercase").checked;
+    const includeNumbers = document.getElementById("numbers").checked;
+    const includeSpecial = document.getElementById("special").checked;
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const specialChars = "!@#$%^&*()_+{}[]|:;<>,.?/";
+
+    let allChars = "";
+    if (includeUppercase) allChars += uppercaseChars;
+    if (includeLowercase) allChars += lowercaseChars;
+    if (includeNumbers) allChars += numberChars;
+    if (includeSpecial) allChars += specialChars;
+
+    if (allChars === "") {
+        alert("Please select at least one character type.");
+        return;
+    }
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+    }
+    document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+    const passwordField = document.getElementById("password");
+    passwordField.select();
+    alert("Password copied to clipboard!");
+}
+    function reset() {
+            document.getElementById("password").value = "";
+    }
